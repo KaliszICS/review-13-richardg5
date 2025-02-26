147bfc762eac32e6f75c0781b9b21ba1af9deb50
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-26 19:57:22 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PracticeProblem {
 
@@ -6,4 +7,29 @@ public class PracticeProblem {
 
 	}
 
+	public static void swap(ArrayList<String> list, int in1, int in2) {
+		String temp = list.get(in1);
+		list.set(in1, list.get(in2));
+		list.set(in2, temp);
+	}
+
+	public static ArrayList<Double> createArrayList(double[] arr) {
+		ArrayList<Double> list = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++) {
+			list.add(arr[i]);
+		}
+		return list;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < names.length; i++) {
+			map.put(names[i], ages[i]);
+		}
+		return map;
+	}
+
+	public static void increaseAge(HashMap<String, Integer> map, String name) {
+		map.put(name, map.get(name) + 1);
+	}
 }
